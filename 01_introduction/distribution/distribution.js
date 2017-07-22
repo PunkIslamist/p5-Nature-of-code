c84070e225bf2101f8bcaf5a6ae42bc3e5c2e9ff
PunkIslamist <achondrias@gmail.com>
2017-07-22 12:53:20 +0200

    Create: random distribution bars.

--- 01_introduction/distribution/distribution.js
+++ 01_introduction/distribution/distribution.js
@@ -1,5 +1,21 @@
+var randomCounts
+
 function setup() {
+   createCanvas(640, 480)
+   randomCounts = Array(20).fill(0)
 }
 
 function draw() {
+   var index = floor(random(randomCounts.length))
+   randomCounts[index]++
+
+   background(123)
+   stroke(200, 100, 50)
+   fill(175)
+
+   var barWidth = width/randomCounts.length
+
+   randomCounts.forEach(function (bar, i) {
+      rect(i * barWidth, height - bar, barWidth - 1, bar)
+   })
 }
