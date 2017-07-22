c954d94948c767d55ce5d0034c1b91b3cc510315
PunkIslamist <achondrias@gmail.com>
2017-07-22 13:29:22 +0200

    Created a biased walker. It's orange. And amazing.

---
+++ 01_introduction/exercises/01_biasedWalker/walker.js
@@ -0,0 +1,30 @@
+var walker
+
+function setup() {
+   createCanvas(640, 480)
+   walker = new Walker()
+   background(123)
+}
+
+function draw() {
+   walker.step()
+   walker.render()
+}
+
+function Walker() {
+   this.x = width/2
+   this.y = height/2
+
+   this.render = function() {
+      stroke(230, 90, 80)
+      point(this.x, this.y)
+   }
+
+   this.step = function() {
+      this.x += floor(random(-1,2, 2))
+      this.x = constrain(this.x, 0, width-1)
+
+      this.y += floor(random(-1, 2,2))
+      this.y = constrain(this.y, 0, height-1)
+   }
+}
