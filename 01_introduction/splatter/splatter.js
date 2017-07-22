db30844cfdc7b79e7bc8f4e2ebf78578bdc98df6
PunkIslamist <achondrias@gmail.com>
2017-07-23 00:02:48 +0200

    Changed the value for the splatter around a bit.

--- 01_introduction/splatter/splatter.js
+++ 01_introduction/splatter/splatter.js
@@ -1,11 +1,15 @@
 function setup() {
    createCanvas(640, 480)
-   background(123)
+   background(0)
 }
 
 function draw() {
    noStroke()
-   fill(40, 120, 240, 20)
+   let red = randomGaussian(180, 40)
+   let green = randomGaussian(128, 40)
+   let blue = randomGaussian(128, 40)
+
+   fill(red, green, blue, 20)
 
    let x = randomGaussian(mouseX, 10)
    let y = randomGaussian(mouseY, 10)
