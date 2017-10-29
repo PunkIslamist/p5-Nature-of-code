1f5531c60d07caa722536b2719086fdbec47487f
PunkIslamist <achondrias@gmail.com>
2017-10-29 12:11:28 +0100

    Created bouncing ball using vectors.

---
+++ 01_introduction/vectors/bouncingBall.js
@@ -0,0 +1,34 @@
+var walker
+
+function Walker() {
+  this.location = createVector(random(width), random(height))
+  this.velocity = createVector(random(-2, 2), random(-2, 2))
+
+  this.move = function () {
+    this.location.add(this.velocity)
+
+    if (this.location.x > width || this.location.x < 0) {
+      this.velocity.x *= -1
+    }
+
+    if (this.location.y > height || this.location.y < 0) {
+      this.velocity.y *= -1
+    }
+  }
+
+  this.display = function () {
+    stroke(40, 50, 180)
+    ellipse(this.location.x, this.location.y, 10)
+  }
+}
+
+function setup() {
+  createCanvas(1280, 600)
+  walker = new Walker()
+}
+
+function draw() {
+  background(100)
+  walker.move()
+  walker.display()
+}
