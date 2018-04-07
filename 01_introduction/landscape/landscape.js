84c33cbb3d5e33a17dce939abc0a7100367cfa22
PunkIslamist <achondrias@gmail.com>
2018-04-07 17:02:49 -0500

    Flagstripes flowing in the wind!

--- 01_introduction/landscape/landscape.js
+++ 01_introduction/landscape/landscape.js
@@ -1,24 +1,48 @@
+var plane
+
 function setup() {
   createCanvas(640, 480)
+  plane = new Plane()
 }
 
 function draw() {
-  background(150, 80, 120)
-  fill(230, 80, 100)
+  background(80, 80, 80)
+  noFill()
+  plane.render()
+}
 
-  let rectangleCount = 10
-  let rectangleWidth = width / rectangleCount
-  let rectangleHeight = height / rectangleCount
+function Plane() {
+  let cellsPerRow = 10
+  let cellHeight = height / cellsPerRow
+  let cellWidth = width / cellsPerRow
+  let rows = createRows(cellsPerRow, cellHeight, cellWidth)
 
-  for (let j = 0; j < rectangleCount; j++) {
+  this.render = function () { rows.forEach(r => r.render()) }
+}
+
+function Row(nr, cellHeight, cellWidth) {
+  let cellCount = width / cellWidth
+  var noiseValue = random(10000)
+
+  this.render = function () {
+    fill(10, 100, nr * 12)
     beginShape(QUAD_STRIP)
-    for (let i = 0; i <= rectangleCount; i++) {
-      let x = i * rectangleWidth
-      let y = j * rectangleHeight
+    for (let i = 0; i <= cellCount; i++) {
+      let h = cellHeight / 2
+      let n = noise(noiseValue + i * 0.1)
+      let m = map(n, 0, 1, -h, h)
+      let x = i * cellWidth
+      let y = nr * cellHeight + m
 
       vertex(x, y)
-      vertex(x, y + rectangleHeight)
+      vertex(x, y + cellHeight)
     }
     endShape()
+    noiseValue += 0.007
   }
+}
+
+function createRows(count, cellHeight, cellWidth) {
+  return Array.apply(null, Array(count))
+    .map((x, i) => new Row(i, cellHeight, cellWidth))
 }
